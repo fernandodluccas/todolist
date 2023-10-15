df211bac25e86aadefc2ac2cb389c40389541b5a
Fernando D'luccas <dluccash@hotmail.com>
2023-10-15 14:58:33 -0300

    Aula 02

--- src/main/java/com/example/todolist/user/UserController.java
+++ src/main/java/com/example/todolist/user/UserController.java
@@ -1,16 +1,29 @@
 package com.example.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.username);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.badRequest().body("User already exists");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.ok(userCreated);
     }
 }
